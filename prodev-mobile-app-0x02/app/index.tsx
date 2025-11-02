4942ddba7ced763ce7f49d0f6877221d6aae2c31
Nurudin Wahab Lartey <nurundin2010@gmail.com>
2025-11-02 07:24:49 +0000

    fix 02

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -1,118 +1,43 @@
-import { StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, ImageBackground, Dimensions, TouchableOpacity } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    position: 'relative'
-  },
-  navGroup: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-    marginTop: 11
-  },
-  largeText: {
-    fontSize: 40,
-    fontWeight: 700,
-  },
-  smallText: {
-    fontSize: 12,
-    color: '#7E7B7B'
-  },
-  placeholderText: {
-    fontSize: 18,
-    color: '#7E7B7B',
-    marginBottom: 7
-  },
-  inputField: {
-    borderWidth: 1,
-    borderRadius: 10,
-    height: 50,
-    borderColor: '#7E7B7B',
-    paddingHorizontal: 10
-  },
-  passwordGroup: {
-    flexDirection: 'row',
-    borderWidth: 1,
-    alignItems: 'center',
-    height: 50,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    borderColor: '#7e7b7b'
-  },
-  formGroup: {
-    marginTop: 44
-  },
-  forgotPasswordText: {
-    fontSize: 17,
-    marginTop: 9,
-    textAlign: 'right',
-    color: '#34967C'
-  },
-  button: {
-    backgroundColor: '#2B876E',
-    height: 53,
-    borderRadius: 10,
-    marginTop: 25,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  buttonText: {
-    fontSize: 17,
-    color: 'white'
-  },
-  socialMediaButton: {
-    borderWidth: 1,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10
-  },
-  socialMediaButtonText: {
-    fontSize: 18,
-    color: '#0D0D0D',
-    fontWeight: 400
-  },
-  socialMediaButtonGroup: {
-    gap: 15,
-    marginTop: 25
-  },
-  dividerGroup: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 10,
-    marginTop: 29
-  },
-  divider: {
-    borderWidth: 0.5,
-    borderColor: "#c2c2c2",
-    flex: 1
-  },
-  dividerText: {
-    fontSize: 17,
-    color: "#C2C2C2",
-  },
-  subTextGroup: {
-    flexDirection: 'row',
-    position: 'absolute',
-    bottom: 33,
-    left: 77,
-    right: 76
-  },
-  subText: {
-    fontSize: 18,
-    color: '#b5b5b5'
-  },
-  subTextJoin: {
-    fontSize: 18,
-    color: '#FFA800',
-    fontWeight: 600
-  }
-});
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground
+          source={require("@/assets/images/background-image.png")}
+          style={styles.background}
+          resizeMode="cover"
+        >
+          <View style={styles.container}>
+            <View style={styles.companyLogo}>
+              <Image source={require("@/assets/images/Logo.png")} />
+            </View>
 
+            <View style={styles.textGroup}>
+              <Text style={styles.textLarge}>Find your favorite place here</Text>
+              <Text style={styles.textSmall}>The best prices for over 2 </Text>
+              <Text style={styles.textSmall}>million properties worldwide</Text>
+            </View>
 
-export {
-  styles
+            <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+              <View style={styles.buttonGroup}>
+                <TouchableOpacity style={styles.button}>
+                  <Text style={{ ...styles.textSmall, color: "black" }}>Join here</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.transparentButton}>
+                  <Text style={styles.textSmall}>Sign In</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ alignItems: "center", paddingVertical: 20 }}>
+                <Text style={{ color: "white" }}>Continue to home</Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
